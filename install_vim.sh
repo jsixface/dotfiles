f101068a46cce438453f30d3277ba7ad0e78e5c7
Arumugam J <sixface@msn.com>
2020-02-01 15:54:17 -0500

    Adjustment for Vim from NeoVim

--- install_vim.sh
+++ install_vim.sh
@@ -6,12 +6,13 @@ _gitdir=`pwd -P`
 [[ -d ~/.config ]] || mkdir ~/.config
 ln -sf ${_gitdir}/vims ~/.config/nvim
 
-which nvim && _nvim_installed=yes
+which nvim > /dev/null && _nvim_installed=yes
 
 if [[ -z $_nvim_installed ]]
 then
     # NeoVim not installed
     echo NeoVim not installed 
+    ln -s ${_gitdir}/vims/init.vim ~/.vimrc
 fi
 
 
