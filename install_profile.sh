7064c6b8bf50ab1b578510ce8cf55aca2611f87b
Arumugam J <sixface@msn.com>
2020-05-13 14:23:31 -0400

    Adding dircolors. To disable annoying directory highlight

--- install_profile.sh
+++ install_profile.sh
@@ -7,6 +7,7 @@ _curr_dir=$(pwd -P)
 ln -vsf ${_curr_dir}/bashrc ~/.bashrc
 ln -vsf ${_curr_dir}/bash_profile ~/.bash_profile
 ln -vsf ${_curr_dir}/bash_aliases ~/.bash_aliases
+cp ${_curr_dir}/dircolors ~/.dircolors
 
 
 ln -vsf ${_curr_dir}/gitconfig ~/.gitconfig
