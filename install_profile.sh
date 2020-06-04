02c36379a81405dce0e8e1ed67beb67f4e9cf230
Arumugam J <sixface@msn.com>
2020-06-04 19:50:48 -0400

    Adding zsh auto-suggestion and fzf

--- install_profile.sh
+++ install_profile.sh
@@ -16,6 +16,10 @@ if [[ -d ~/.oh-my-zsh ]]
 then
 	ln -vsf ${_curr_dir}/zsh/aliases.zsh ~/.oh-my-zsh/custom/aliases.zsh 
 	ln -vsf ${_curr_dir}/zsh/custom.zsh ~/.oh-my-zsh/custom/custom.zsh
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	pos=$(grep -n ^plugins= ~/.zshrc | cut -d : -f 1)
+	sed -i "${pos}a\plugins+=(zsh-autosuggestions z)" ~/.zshrc
 fi
 
-
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
