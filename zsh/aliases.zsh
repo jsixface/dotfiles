3a43ac9ca6825e093f8d366c00ebdbd611260dcb
Arumugam J <sixface@msn.com>
2020-12-11 11:04:48 -0500

    betterer code

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,6 +5,16 @@ alias ..='cd ..'
 alias o='less'
 alias c=clear
 
+alias l='ls -lFh'     #size,show type,human readable
+alias la='ls -lAFh'   #long list,show almost all,show type,human readable
+alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
+alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
+alias ll='ls -l'      #long list
+alias ldot='ls -ld .*'
+alias lS='ls -1FSsh'
+alias lart='ls -1Fcart'
+alias lrt='ls -1Fcrt'
+
 alias ping="ping -c 4"
 alias untar="tar -xvf"
 alias untarz="tar -xzvf"
@@ -20,13 +30,16 @@ then
     alias ls="ls -G"
 fi
 
+alias -g G='| grep'
+alias -g L="| less"
+alias -g LL="2>&1 | less"
 
 alias gca='git commit -a'
 #alias git='git --no-pager'
 alias gits='git status -bs'
 alias gst='git status -bs'
 alias glo='git log --oneline --decorate -10'
-alias yolo='git commit -am "betterer code"'
+alias yolo='git add .; git commit -am "betterer code"'
 
 alias ubuntu-update='sudo apt update && apt list --upgradable && sudo apt upgrade -y && sudo apt autoremove -y'
 alias rf="rm -rf"
@@ -47,3 +60,7 @@ function mkcd() {
 	mkdir -p $1
 	cd $1
 }
+
+
+alias zshrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zshrc' # Quick access to the .zshrc file
+alias grep='grep --color'
